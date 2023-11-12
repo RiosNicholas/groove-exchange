e54e4ff559e335bc9c3860e2413a1986ce761066
Nicholas Rios <rios.nicholas@protonmail.com>
2023-11-12 18:07:13 -0500

    Update header text and remove logo

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,17 +2,14 @@ import Logo from '../../public/record.svg';
 const NavBar = () => {
     return (
         <header className="flex justify-evenly items-center top-0 w-screen px-5 py-1 bg-neutral-800">
-            <img 
-                src={Logo} 
-                className='max-h-12 mx-5 my-1'
-            />
+            <h1 className='max-h-12 mx-5 my-1 font-black text-xl uppercase'>Groove Exchange</h1>
             <input
                 type="text" 
                 placeholder="Search" 
                 className="p-1 mx-5 my-1 w-1/3 rounded-md font-medium"
             />
             <nav className="p-1 mx-5 my-1">
-                <ul className="flex gap-4 text-lg font-bold">
+                <ul className="flex gap-4 text-base font-bold">
                     <li className="hover:cursor-pointer hover:text-purple-300"><a>Home</a></li>
                     <li className="hover:cursor-pointer hover:text-purple-300"><a>Create New Post</a></li>
                 </ul>
