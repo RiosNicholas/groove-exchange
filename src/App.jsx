0b42495f2dde6f9b06d0c4d56effe0385173f6e3
Nicholas Rios <rios.nicholas@protonmail.com>
2023-11-12 22:41:05 -0500

    Refactor React Router configuration

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <>
       <NavBar />
-      <PostFeed />
+      <PostFeed/>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -7,17 +7,17 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import NavBar from './components/NavBar.jsx';
 import EditPost from './routes/EditPost.jsx'
 import CreatePost from './routes/CreatePost.jsx'
-
+import Home from './routes/Home.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-    <Routes>
-        <Route path='/' element={<App />}>
-        <Route index={true} path='/create-post' element={<CreatePost />} />
-        <Route index={true} path='/edit-post/:id' element={<EditPost />} />
-      </Route>
-    </Routes>
+      <NavBar />
+      <Routes>
+          <Route index={true} path='/' element={<Home />} />
+          <Route index={true} path='/create-post' element={<CreatePost />} />
+          <Route index={true} path='/edit-post/:id' element={<EditPost />} />
+      </Routes>
     </BrowserRouter>
-  </React.StrictMode>,
+  </React.StrictMode>
 )
