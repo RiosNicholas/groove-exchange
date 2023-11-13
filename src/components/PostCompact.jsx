dfadbf054fbed67bc2024bbf9b0a7126d5d34acf
Nicholas Rios <rios.nicholas@protonmail.com>
2023-11-12 20:20:20 -0500

    Update background to tailwind neutral

--- src/components/PostCompact.jsx
+++ src/components/PostCompact.jsx
@@ -1,6 +1,6 @@
 const PostCompact = ({ title, numUpvotes, timePosted }) => {
     return (
-        <div className="bg-white text-black rounded p-5 m-2">
+        <div className="bg-neutral-100 text-black rounded p-5 m-2">
             <p className="text-neutral-700 text-sm font-light">Posted {timePosted} ago</p>
             <h2 className="font-extrabold text-xl mb-1">dd{title}</h2>
             <p className="text-neutral-700 font-normal text-base">{numUpvotes} upvotes</p>
