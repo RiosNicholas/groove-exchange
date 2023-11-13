2fdb42f7826fc920d0b5c463673e036919a6411b
Nicholas Rios <rios.nicholas@protonmail.com>
2023-11-12 19:57:49 -0500

    Update button colors to tailwind lime

--- src/components/FeedFilter.jsx
+++ src/components/FeedFilter.jsx
@@ -4,8 +4,8 @@ const FeedFilter = () => {
             <p className="text-white text-sm m-1">
                 Order by: 
                 <span>
-                    <button className="bg-green-700 font-bold p-1 m-1 hover:border-neutral-400">Newest</button>
-                    <button className="bg-lime-700 font-bold p-1 m-1 hover:border-neutral-400">Most Popular</button>
+                    <button className="bg-lime-700 font-bold p-1 m-1 hover:border-neutral-400">Newest</button>
+                    <button className="bg-lime-600 font-bold p-1 m-1 hover:border-neutral-400">Most Popular</button>
                 </span>
             </p>
         </div>
