50b88be9089a89d810c68c9bba21b895c13a01b5
Nicholas Rios <rios.nicholas@protonmail.com>
2023-11-15 00:01:46 -0500

    Add exception handling

--- src/components/PostFeed.jsx
+++ src/components/PostFeed.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import { Routes, Route, useRoutes, Link } from "react-router-dom";
 import { supabase } from '../client'
 import FeedFilter from "./FeedFilter";
-import PostCompactList from "./PostListCompact";
+import PostCompactList from "./PostCompactList";
 import PostDetailed from "./PostDetailed";
 
 const PostFeed = () => {
@@ -15,20 +15,29 @@ const PostFeed = () => {
     
     useEffect(() => {
         const fetchPosts = async () => {
-            const {data} = await supabase
-                .from('Posts')
-                .select()
-                .order('created_at', { ascending: true })
+            try {
+                const { data, error } = await supabase
+                    .from('Posts')
+                    .select()
+                    .order('created_at', { ascending: true });
+            
+                if (error) {
+                    console.error('Error fetching data:', error);
+                } else {
+                    setPosts(data);
+                }
+            } catch (error) {
+                console.error('Error in fetchPosts:', error);
+            }
+        }; 
       
-            setPosts(data);
-            };
-      
-            if(sessionStorage.getItem('token')){
-                let data = JSON.parse(sessionStorage.getItem('token'));
-                setToken(data);
-            };
-      
-          fetchPosts();
+        if(sessionStorage.getItem('token')){
+            let data = JSON.parse(sessionStorage.getItem('token'));
+            setToken(data);
+        };
+
+        console.log(posts)
+        fetchPosts();
     }, []);
     
     return (
