1874672788e2fb713cb6e453a741068ae2f5ddd9
Nicholas Rios <rios.nicholas@protonmail.com>
2023-11-14 20:49:51 -0500

    Update CSS and add upvote buttons

--- src/components/PostDetailed.jsx
+++ src/components/PostDetailed.jsx
@@ -1,20 +1,38 @@
 import { useState } from "react";
 
 const PostDetailed = ({ title, description, numUpvotes, timePosted, imgUrl, comments }) => {
-    const [upvotes, setUpvotes] = useState(numUpvotes);
+    const [upvotes, setUpvotes] = useState(0);
+
+    const handleUpvote = () => {
+        setUpvotes(upvotes + 1);
+    };
+
+    const handleDownvote = () => {
+        setUpvotes(upvotes - 1);
+    };
 
     return (
         <div className="bg-neutral-100 mx-7 my-2 rounded">
-            <main className="bg-neutral-100 text-black rounded p-5 my-4">
+            <div className="flex items-center ">
+            <aside className="flex flex-col  text-2xl">
+                <button onClick={handleUpvote} className="hover:bg-neutral-200 bg-neutral-100">
+                    <span role="img" aria-label="upvote">👍</span>
+                </button>
+                <button onClick={handleDownvote} className="hover:bg-neutral-200 bg-neutral-100">
+                    <span role="img" aria-label="downvote">👎</span>
+                </button>
+            </aside>
+            <main className="bg-neutral-100 text-black rounded py-5 px-1 my-4">
                 <p className="text-neutral-700 text-sm font-light mb-1">Posted {timePosted} ago</p>
-                <h2 className="font-extrabold text-2xl">{title}itle</h2>
+                <h2 className="font-extrabold text-2xl">{title}Title</h2>
                 <p className="text-lg">{description}description</p>
                 <img 
                     src={imgUrl} 
                     className="max-w-lg mb-1"
                 />
-                <p className="text-neutral-700 font-normal text-base">{numUpvotes} upvotes</p>
+                <p className="text-neutral-700 font-normal text-base">{upvotes} upvotes</p>
             </main>
+            </div>
             <div className="flex flex-col bg-neutral-500 px-8 p-2">
                 <ul className="list-disc list-inside">
                     <li>{comments}comment</li>
